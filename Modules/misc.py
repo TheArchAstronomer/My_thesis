32125fc313f91504672daf1af5e21f474b1de7bc
TheArchAstronomer <dnurme@gmail.com>
2026-04-16 12:23:11 +0300

    Big Update

--- Modules/misc.py
+++ Modules/misc.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.ndimage import gaussian_filter
 from matplotlib.colors import Normalize, PowerNorm
+from matplotlib import colors
 #import plotly.graph_objects as go
 
 def overdensity(x, y, bins):  # generating the overdensity map
@@ -21,7 +22,7 @@ def plot_OD_gaussian(x, y, bins, sigma, xaxis, yaxis): # x coord, y coord, nr of
     
     OD, xedges, yedges = overdensity(x, y, bins) # calling out the function
     hist_smoothed = gaussian_filter(OD.T, sigma=sigma)
-    im = ax.imshow(hist_smoothed, origin='upper', cmap = "viridis", 
+    im = ax.imshow(hist_smoothed, origin='upper', cmap = "seismic", 
                    extent=[xedges[0], xedges[-1], yedges[0], yedges[-1]], 
                    norm=Normalize(vmin=-np.max(np.abs(hist_smoothed)), vmax=1.0, clip=False))
     ax.scatter(0,0, color='white', marker='x', s=150, linewidths=3,label='LMC')
@@ -134,3 +135,45 @@ def plot_gaussian_interactive(x, y, bins, xaxis, yaxis):
 
     fig.show()
     return fig
+
+
+def resultant_OD_gaussian(data1, data2, bins, sigma, data1_label, data2_label):
+    
+    def overdensity(x, y, bins):  # generating the overdensity map
+        pre_OD, xedges, yedges = np.histogram2d(x, y, bins)
+        OD = ((pre_OD / np.mean(pre_OD))-1)
+        return OD, xedges, yedges
+    
+    x1, y1, z1 = data1[:, 0], data1[:, 1], data1[:, 2]
+    x2, y2, z2 = data2[:, 0], data2[:, 1], data2[:, 2]
+
+    OD1, xedges1, yedges1 = overdensity(x1, y1, bins)
+    OD2, xedges2, yedges2 = overdensity(x2, y2, bins)
+
+    hist_smoothed1 = gaussian_filter(OD1.T, sigma=sigma)
+    hist_smoothed2 = gaussian_filter(OD2.T, sigma=sigma)
+
+    resultant_OD = hist_smoothed1 - hist_smoothed2
+
+    fig, axs = plt.subplots(1, 3, figsize=(18, 6), facecolor='white')
+    im1 = axs[0].imshow(hist_smoothed1, origin='upper', cmap="seismic", 
+                        extent=[xedges1[0], xedges1[-1], yedges1[0], yedges1[-1]],
+                          norm=Normalize(vmin=-np.max(np.abs(hist_smoothed1)), vmax=np.max(np.abs(hist_smoothed1)),
+                                          clip=False))
+    im2 = axs[1].imshow(hist_smoothed2, origin='upper', cmap="seismic", 
+                        extent=[xedges2[0], xedges2[-1], yedges2[0], yedges2[-1]],
+                          norm=Normalize(vmin=-np.max(np.abs(hist_smoothed2)), vmax=np.max(np.abs(hist_smoothed2)),
+                                          clip=False))
+    im3 = axs[2].imshow(resultant_OD, origin='upper', cmap="seismic", 
+                        extent=[xedges1[0], xedges1[-1], yedges1[0], yedges1[-1]],
+                          norm=Normalize(vmin=-np.max(np.abs(resultant_OD)), vmax=np.max(np.abs(resultant_OD)), clip=False))
+    c = plt.colorbar(im1, ax=axs[0])
+    c = plt.colorbar(im2, ax=axs[1])
+    c = plt.colorbar(im3, ax=axs[2])
+    axs[0].set_title(f'{data1_label} Overdensity', size=16)
+    axs[1].set_title(f'{data2_label} Overdensity', size=16)
+    axs[2].set_title('Resultant Overdensity', size=16)
+
+    return fig, axs
+
+
